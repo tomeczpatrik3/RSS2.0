cc7021ac809310c62595b29e6f9ae5ad033c850e
Tomecz Patrik <tomeczp@computrend.local>
2018-03-27 11:06:07 +0200

    reservation validation messages fixed

--- backend/src/main/java/RoomReservationSystem/config/ValidationErrorMessageConstants.java
+++ backend/src/main/java/RoomReservationSystem/config/ValidationErrorMessageConstants.java
@@ -1,7 +1,6 @@
 package RoomReservationSystem.config;
 
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 
 /**
  * A validálással kapcsolatos hibaüzeneteket tartalmazó osztály
@@ -9,6 +8,7 @@ import org.springframework.validation.ObjectError;
  */
 public class ValidationErrorMessageConstants {
     //CLASSROOM:
+    public static final String CLASSROOM_EMPTY = "A tantermet meg kell adni!";
     public static final String CLASSROOM_NAME_EMPTY = "A tanterem neve nem lehet üres!";
     public static final String CLASSROOM_HAS_PC_EMPTY = "A PC nem lehet üres!";
     public static final String CLASSROOM_HAS_PROJECTOR_EMPTY = "A projektor neve nem lehet üres!";
@@ -20,6 +20,7 @@ public class ValidationErrorMessageConstants {
     public static final String CLASSROOM_CHAIRS_VALUE = "A székek száma nem nagyobb mint nulla!";
     
     //BUILDING:
+    public static final String BUILDING_EMPTY = "Az épületet meg kell adni!";
     public static final String BUILDING_ALREDY_EXISTS = "Ezzel a névvel már létezik épület!";
     public static final String BUILDING_NOT_EXISTS = "Nem létezik ilyen épület!";
     public static final String BUILDING_NAME_SIZE = "Az épület neve nem megfelelő hosszúságú (3-30)!";
@@ -28,20 +29,10 @@ public class ValidationErrorMessageConstants {
     //RESERVATION:
     public static final String RESERVATION_NOT_EXISTS = "Ilyen foglalás nem létezik!";
     public static final String RESERVATION_ALREDY_EXISTS = "Ilyen foglalás már létezik!";
-    public static final String RESERVATION_DAY_EMPTY = "A nap nem lehet üres!";
-    public static final String RESERVATION_START_TIME_EMPTY = "A kezdeti idő nem lehet üres!";
-    public static final String RESERVATION_END_TIME_EMPTY = "A befejezési idő nem lehet üres!";
-    public static final String RESERVATION_START_DATE_EMPTY = "A kezdeti dátum nem lehet üres!";
-    public static final String RESERVATION_END_DATE_EMPTY = "A befejezési dátum nem lehet üres!";
-    public static final String RESERVATION_DAY_SIZE = "A nap nem megfelelő hosszúságú (3-12)!";
-    public static final String RESERVATION_START_DATE_INVALID = "A kezdeti dátum nem megfelelő formátumú!";
-    public static final String RESERVATION_END_DATE_INVALID = "A befejezési dátum nem megfelelő formátumú!";
-    public static final String RESERVATION_END_DATE_BEFORE_START_DATE = "A befejezési dátum nem lehet korábbi, mint a kezdési dátum!";
-    public static final String RESERVATION_START_TIME_INVALID = "A kezdeti idő nem megfelelő formátumú!";
-    public static final String RESERVATION_END_TIME_INVALID = "A befejezési idő nem megfelelő formátumú!";
-    public static final String RESERVATION_END_TIME_BEFORE_START_TIME = "A befejezési idő nem lehet korábban, mint a kezdési idő!";
+    public static final String RESERVATION_DATES_EMPTY = "A foglaláshoz tartozó időpontokat meg kell adni!";
     
     //SUBJECT:
+    public static final String SUBJECT_EMPTY = "A tantárgyat meg kell adni!";
     public static final String SUBJECT_ALREDY_EXISTS = "Ezzel a kóddal már létezik tantárgy!";
     public static final String SUBJECT_NOT_EXISTS = "Nem létezik ilyen tantárgy!";
     public static final String SUBJECT_NAME_SIZE = "A tantárgy neve nem megfelelő hosszúságú (3-30)!";
@@ -50,6 +41,7 @@ public class ValidationErrorMessageConstants {
     public static final String SUBJECT_CODE_EMPTY = "A tantárgy kódja nem lehet üres!";    
     
     //USER:
+    public static final String USER_EMTPY = "A felhasználót meg kell adni!";
     public static final String USER_USERNAME_EMPTY = "A felhasználónév nem lehet üres!";
     public static final String USER_NAME_EMPTY = "A név nem lehet üres!";
     public static final String USER_PASSWORD_EMPTY = "A jelszó nem lehet üres!";
@@ -66,6 +58,7 @@ public class ValidationErrorMessageConstants {
     public static final String USER_EMAIL_INVALID = "Az e-mail cím formátuma nem megfelelő!";
     
     //SEMESTER:
+    public static final String SEMESTER_EMPTY = "A félévet meg kell adni!";
     public static final String SEMESTER_NAME_EMPTY = "A félév neve nem lehet üres!";
     public static final String SEMESTER_START_DATE_EMPTY = "A félév kezdetének dátuma nem lehet üres!";
     public static final String SEMESTER_END_DATE_EMPTY = "A félév végének dátuma nem lehet üres!";
@@ -86,9 +79,9 @@ public class ValidationErrorMessageConstants {
     public static String concatErrors(BindingResult bindingResult) {
         StringBuilder errors = new StringBuilder();
         errors.append("Hiba történt: \n");
-        for (ObjectError err : bindingResult.getAllErrors()) {
-           errors.append(err.getDefaultMessage());
-        }
+        bindingResult.getAllErrors().forEach((err) -> {
+            errors.append(err.getDefaultMessage());
+        });
         return errors.toString();
     }
 }
--- backend/src/main/java/RoomReservationSystem/validation/ReservationValidator.java
+++ backend/src/main/java/RoomReservationSystem/validation/ReservationValidator.java
@@ -6,9 +6,6 @@ import RoomReservationSystem.service.SubjectService;
 import RoomReservationSystem.service.UserService;
 import static RoomReservationSystem.config.ValidationErrorMessageConstants.*;
 
-import java.text.SimpleDateFormat;
-import java.util.regex.Pattern;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
@@ -43,12 +40,11 @@ public class ReservationValidator implements Validator {
      */
     @Override
     public void validate(Object target, Errors errors) {
-        ValidationUtils.rejectIfEmpty(errors, "username", "reservation.username.empty", USER_USERNAME_EMPTY);
-        ValidationUtils.rejectIfEmpty(errors, "subjectCode", "reservation.subjectCode.empty", SUBJECT_CODE_EMPTY);
-        ValidationUtils.rejectIfEmpty(errors, "roomName", "reservation.roomName.empty", CLASSROOM_NAME_EMPTY);
-        ValidationUtils.rejectIfEmpty(errors, "day", "reservation.day.empty", RESERVATION_DAY_EMPTY);
-        ValidationUtils.rejectIfEmpty(errors, "startTime", "reservation.startTime.empty", RESERVATION_START_TIME_EMPTY);
-        ValidationUtils.rejectIfEmpty(errors, "endTime", "reservation.endTime.empty", RESERVATION_END_TIME_EMPTY);
+        ValidationUtils.rejectIfEmpty(errors, "semester", "reservation.semester.empty", SEMESTER_EMPTY);
+        ValidationUtils.rejectIfEmpty(errors, "user", "reservation.user.empty", USER_EMTPY);
+        ValidationUtils.rejectIfEmpty(errors, "subject", "reservation.subject.empty", SUBJECT_EMPTY);
+        ValidationUtils.rejectIfEmpty(errors, "classroom", "reservation.classroom.empty", CLASSROOM_EMPTY);
+        ValidationUtils.rejectIfEmpty(errors, "dates", "reservation.dates.empty", RESERVATION_DATES_EMPTY);
 
         ReservationDTO res = (ReservationDTO)target;     
 
